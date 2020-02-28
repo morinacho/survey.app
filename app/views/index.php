fe37cb1a55d2fcd60909c04c8a80f7602c7efb92
Ignacio Morinigo <morinacho@gmail.com>
2020-02-28 12:46:55 -0300

    Login

--- app/views/index.php
+++ app/views/index.php
@@ -1,11 +1,12 @@
-<?php 
-	require_once APP_ROUTE . '/views/modules/header.php'; 
-	if (!Controller::authenticated()){
+<?php  
+	session_start(); 
+	if (Controller::authenticated()){
+		require_once APP_ROUTE . '/views/modules/header.php';
 		require_once APP_ROUTE . '/views/modules/home.php';
+		require_once APP_ROUTE . '/views/modules/footer.php';  
 	}
  	else{
 		require_once APP_ROUTE . '/views/modules/login.php'; 
-	 }  
-    	
-	require_once APP_ROUTE . '/views/modules/footer.php'; 
+	 }   
+	
 ?>
--- app/views/modules/nav.php
+++ app/views/modules/nav.php
@@ -1,5 +1,4 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  
   <a class="navbar-brand" href="<?php echo URL_ROUTE ?>home">
     <img src="<?php echo URL_ROUTE ?>media/icons/logo.png" width="30" height="30" class="d-inline-block align-top" alt=""> 
     Survey app
@@ -8,25 +7,20 @@
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+  <?php if (Controller::authenticated()) : ?>
     <ul class="navbar-nav">
       <li class="nav-item">
-        <?php if (Controller::authenticated()) : ?>
-          <a class="nav-link" data-toggle="modal" data-target=".login-modal" href="javascript:void(0);">Login</a>
-        <?php else :?>
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><?php #echo $_SESSION['user'] ?></a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle material-icons" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
-            settings
-            </a>
-            <div class="dropdown-menu dropdown-menu-right"> 
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><?php echo $_SESSION['user'] ?></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle material-icons" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">settings</a>
+          <div class="dropdown-menu dropdown-menu-right"> 
             <a class="dropdown-item" href="<?php echo URL_ROUTE ?>surveys/config">Configuración</a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Salir</a>
-            </div>
-        <?php endif; ?>
+            <a class="dropdown-item" href="<?php echo URL_ROUTE ?>auth/logout">Salir</a>
+          </div>
       </li>
     </ul>
+  <?php endif; ?>
   </div>
-</nav>
-<?php require_once APP_ROUTE . '/views/modules/register.php'; ?>
+</nav> 
